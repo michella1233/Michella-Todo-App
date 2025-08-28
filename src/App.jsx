399238cfcbc88e81cb57c8f9f4a719d409d5b206
michella1233 <michellalaoh@gmail.com>
2025-08-29 02:01:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import profilePicture from './assets/avatar4.png'
 import sun from './assets/sun.png'
 import moon from './assets/moon.png'
+import hamburger from './assets/hamburger.png'
+import hamburgerDark from './assets/hamburgerDark.png'
 import { TodoContent } from './TodoContent'
 import { CreateNewList } from './CreateNewTitle'
 import { TodoHeader } from './TodoHeader'
@@ -12,7 +14,7 @@ function App() {
   const [todos, setTodos] = useState(() => {
     const saved = localStorage.getItem('todos');
     return saved ? JSON.parse(saved) : [];
-  }); 
+  });
   const [title, setTitle] = useState(() => {
     const saved = localStorage.getItem('title');
     return saved ? JSON.parse(saved) : "";
@@ -34,26 +36,36 @@ function App() {
 
   const [render, setRender] = useState(null)
 
- const [isDarkMode, setIsDarkMode] = useState(()=>{
+  const [isDarkMode, setIsDarkMode] = useState(() => {
     const saved = localStorage.getItem("isDarkMode");
     return saved ? JSON.parse(saved) : false;
   });
 
- useEffect(() => {
-  if (isDarkMode) {
-    document.body.classList.add("dark");
-  } else {
-    document.body.classList.remove("dark");
-  };
-  localStorage.setItem('isDarkMode', isDarkMode)
-}, [isDarkMode]);
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    };
+    localStorage.setItem('isDarkMode', isDarkMode)
+  }, [isDarkMode]);
+
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    function toggleSidebar() {
+    setIsSidebarOpen(!isSidebarOpen);
+  }
+
 
   return (
     <div className={"container"}>
-      <div className='left-container'>
+      <div 
+      className={`left-container ${isSidebarOpen ? "active" : ""} ${isDarkMode ? "left-container-dark" : ""}`}  
+      id="sidebar">
         <div className='profile-container'>
           <img className='pp' src={profilePicture} />
-          <p className={isDarkMode ? 'profile-dark': 'profile-light'}>My Todo List</p>
+          <p className={isDarkMode ? 'profile-dark' : 'profile-light'}>My Todo List</p>
         </div>
         <CreateNewList
           title={title}
@@ -68,24 +80,33 @@ function App() {
           render={render}
           setInput={setInput}
           isDarkMode={isDarkMode}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
         />
       </div>
       <div className='right-container'>
+        <img
+          onClick={toggleSidebar}
+          className='hamburger'
+          src={isDarkMode ? hamburgerDark : hamburger}
+        />
         <div className='right-container-header'>
-          <TodoHeader
-            title={title}
-            date={date}
-            setTitle={setTitle}
-            setDate={setDate}
-            headerList={headerList}
-            setHeaderList={setHeaderList}
-            setTodos={setTodos}
-            todoInputRef={todoInputRef}
-            render={render}
-            setRender={setRender}
-            isDarkMode={isDarkMode}
-          />
-          <div className='add-todo'>
+          <div>
+            <TodoHeader
+              title={title}
+              date={date}
+              setTitle={setTitle}
+              setDate={setDate}
+              headerList={headerList}
+              setHeaderList={setHeaderList}
+              setTodos={setTodos}
+              todoInputRef={todoInputRef}
+              render={render}
+              setRender={setRender}
+              isDarkMode={isDarkMode}
+            />
+          </div>
+          <div className='background-mode'>
             <img
               className='sun'
               src={isDarkMode ? moon : sun}
